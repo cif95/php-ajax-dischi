1f7fc940deb619b66e178f734609cca32ce6ae8a
cif95 <vivianacifarelli9@gmail.com>
2022-04-22 15:33:09 +0200

    implemented milestone 1 (albums cards with php)

--- partials/main.php
+++ partials/main.php
@@ -3,5 +3,20 @@
 ; ?>
 
 <main>
-	<h2>Main</h2>
+	<section id="albums-cards" class="container">
+		<?php foreach($albums as $albums) { ; ?>
+			<div class="album-card">
+				<img src="<?= $albums['poster'] ; ?>" alt="<?= $albums['title'] ; ?>" />
+				<h3 class="title">
+					<?= $albums['title'] ; ?>
+				</h3>
+				<p class="author">
+					<?= $albums['author'] ; ?>
+				</p>
+				<span class="year">
+					<?= $albums['year'] ; ?>
+				</span>
+			</div>
+		<?php } ; ?>
+	</section>
 </main>
